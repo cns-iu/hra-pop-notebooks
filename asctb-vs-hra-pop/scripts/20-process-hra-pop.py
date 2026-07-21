55de56f2e438abf790858d83daf86b6cf64c5aca
Andreas Bueckle <abueckle@iu.edu>
2026-07-21 16:24:24 -0400

    Update 20-process-hra-pop.py

--- asctb-vs-hra-pop/scripts/20-process-hra-pop.py
+++ asctb-vs-hra-pop/scripts/20-process-hra-pop.py
@@ -10,9 +10,9 @@ def download_hra_pop_and_get_organ_as_ct_trios() -> None:
         "https://raw.githubusercontent.com/x-atlas-consortia/hra-pop/refs/heads/main/output-data/v1.1/reports/atlas-ad-hoc/cell-types-in-anatomical-structurescts-per-as.csv"
     )
 
-    df_hra_pop_trio = df_hra_pop[
-        ["organ", "as", "as_label", "cell_id", "cell_label"]
-    ]
+    df_hra_pop_trio = df_hra_pop.drop_duplicates(
+        subset=["organ", "as", "as_label", "cell_id", "cell_label"]
+    )[["organ", "as", "as_label", "cell_id", "cell_label"]]
 
     df_hra_pop_trio[["as", "cell_id"]] = df_hra_pop_trio[["as", "cell_id"]].map(
         purl_to_id
